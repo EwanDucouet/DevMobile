9f6508cc3103769bc72fa947f498b8785b9c1728
KAlecs12 <alexandre.bruno@ynov.com>
2022-04-24 22:29:00 +0200

    Push final intégration burger - calendrier - paint - calculatrice

--- app/src/main/java/com/example/projetandroid/CalculatorActivity.kt
+++ app/src/main/java/com/example/projetandroid/CalculatorActivity.kt
@@ -5,155 +5,98 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class CalculatorActivity : AppCompatActivity() {
+    // TextView used to display the input and output
+    lateinit var txtInput: TextView
+
+    // Represent whether the lastly pressed key is numeric or not
+    var lastNumeric: Boolean = false
+
+    // Represent that current state is in error or not
+    var stateError: Boolean = false
+
+    // If true, do not allow to add another DOT
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
-        supportActionBar?.hide()
-        val CalculatorResult = findViewById<TextView>(R.id.CalculatorResult)
-        val button_cancel = findViewById<Button>(R.id.button_cancel)
-        val button_back = findViewById<Button>(R.id.button_back)
-        val button_divide = findViewById<Button>(R.id.button_divide)
-        val button_modulo = findViewById<Button>(R.id.button_modulo)
-        val button_multiply = findViewById<Button>(R.id.button_multiply)
-        val button_minus = findViewById<Button>(R.id.button_minus)
-        val button_plus = findViewById<Button>(R.id.button_plus)
-        val button_comma = findViewById<Button>(R.id.button_comma)
-        val button_0 = findViewById<Button>(R.id.button_0)
-        val button_1 = findViewById<Button>(R.id.button_1)
-        val button_2 = findViewById<Button>(R.id.button_2)
-        val button_3 = findViewById<Button>(R.id.button_3)
-        val button_4 = findViewById<Button>(R.id.button_4)
-        val button_5 = findViewById<Button>(R.id.button_5)
-        val button_6 = findViewById<Button>(R.id.button_6)
-        val button_7 = findViewById<Button>(R.id.button_7)
-        val button_8 = findViewById<Button>(R.id.button_8)
-        val button_9 = findViewById<Button>(R.id.button_9)
-        val button_equal = findViewById<Button>(R.id.button_equal)
-        button_cancel.setOnClickListener {
-            CalculatorResult.setText("");
+        txtInput = findViewById(R.id.txtInput)
+    }
+
+    /**
+     * Append the Button.text to the TextView
+     */
+    fun onDigit(view: View) {
+        if (stateError) {
+            // If current state is Error, replace the error message
+            txtInput.text = (view as Button).text
+            stateError = false
+        } else {
+            // If not, already there is a valid expression so append to it
+            txtInput.append((view as Button).text)
         }
-        button_back.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.substring(0, CalculatorResult.text.length - 1));
+        // Set the flag
+        lastNumeric = true
+    }
+
+    /**
+     * Append . to the TextView
+     */
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !stateError && !lastDot) {
+            txtInput.append(".")
+            lastNumeric = false
+            lastDot = true
         }
-        button_divide.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "/");
+    }
+
+    /**
+     * Append +,-,*,/ operators to the TextView
+     */
+    fun onOperator(view: View) {
+        if (lastNumeric && !stateError) {
+            txtInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false    // Reset the DOT flag
         }
-        button_modulo.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "%");
-        }
-        button_multiply.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "x");
-        }
-        button_minus.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "-");
-        }
-        button_plus.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "+");
-        }
-        button_comma.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + ".");
-        }
-        button_0.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "0");
-        }
-        button_1.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "1");
-        }
-        button_2.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "2");
-        }
-        button_3.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "3");
-        }
-        button_4.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "4");
-        }
-        button_5.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "5");
-        }
-        button_6.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "6");
-        }
-        button_7.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "7");
-        }
-        button_8.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "8");
-        }
-        button_9.setOnClickListener {
-            CalculatorResult.setText(CalculatorResult.text.toString() + "9");
-        }
-        button_equal.setOnClickListener {
-            var result = 0
-            var tempIndex = 0
-            val calcul = CalculatorResult.text.toString()
-            if (calcul != "") {
-                var i = 0
-                while (i < calcul.length) {
-                    if (calcul[i] == '/' && i < calcul.length - 1) {
-                        i += 1
-                        tempIndex = i
-                        while (calcul[i+1].isDigit()) {
-                            i += 1
-                        }
-                        result /= calcul.substring(tempIndex, i + 1).toInt()
-                        tempIndex = 0
-                        i += 1
-                    }
-                    else if (calcul[i] == '%' && i < calcul.length - 1) {
-                        i += 1
-                        tempIndex = i
-                        while (calcul[i+1].isDigit()) {
-                            i += 1
-                        }
-                        result %= calcul.substring(tempIndex, i + 1).toInt()
-                        tempIndex = 0
-                        i += 1
-                    }
-                    else if (calcul[i] == 'x' && i < calcul.length - 1) {
-                        i += 1
-                        tempIndex = i
-                        while (calcul[i+1].isDigit()) {
-                            i += 1
-                        }
-                        result *= calcul.substring(tempIndex, i + 1).toInt()
-                        tempIndex = 0
-                        i += 1
-                    }
-                    else if (calcul[i] == '-' && i < calcul.length - 1) {
-                        i += 1
-                        tempIndex = i
-                        while (calcul[i+1].isDigit()) {
-                            i += 1
-                        }
-                        result -= calcul.substring(tempIndex, i + 1).toInt()
-                        tempIndex = 0
-                        i += 1
-                    }
-                    else if (calcul[i] == '+' && i < calcul.length - 1) {
-                        i += 1
-                        tempIndex = i
-                        while (calcul[i+1].isDigit()) {
-                            i += 1
-                        }
-                        result += calcul.substring(tempIndex, i + 1).toInt()
-                        tempIndex = 0
-                        i += 1
-                    }
-                    else if (calcul[i].isDigit()) {
-                        tempIndex = i
-                        while (calcul[i+1].isDigit()) {
-                            i += 1
-                        }
-                        result += calcul.substring(tempIndex, i + 1).toInt()
-                        tempIndex = 0
-                        i += 1
-                    }
-                }
+    }
+
+
+    /**
+     * Clear the TextView
+     */
+    fun onClear(view: View) {
+        this.txtInput.text = ""
+        lastNumeric = false
+        stateError = false
+        lastDot = false
+    }
+
+    /**
+     * Calculate the output using Exp4j
+     */
+    fun onEqual(view: View) {
+        // If the current state is error, nothing to do.
+        // If the last input is a number only, solution can be found.
+        if (lastNumeric && !stateError) {
+            // Read the expression
+            val txt = txtInput.text.toString()
+            // Create an Expression (A class from exp4j library)
+            val expression = ExpressionBuilder(txt).build()
+            try {
+                // Calculate the result and display
+                val result = expression.evaluate()
+                txtInput.text = result.toString()
+                lastDot = true // Result contains a dot
+            } catch (ex: ArithmeticException) {
+                // Display an error message
+                txtInput.text = "Error"
+                stateError = true
+                lastNumeric = false
             }
-            CalculatorResult.setText(CalculatorResult.text.toString() + "0");
         }
     }
 }
--- app/src/main/java/com/example/projetandroid/MainActivity.kt
+++ app/src/main/java/com/example/projetandroid/MainActivity.kt
@@ -1,21 +1,52 @@
 package com.example.projetandroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
-        toggle.isDrawerIndicatorEnabled = true
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+         drawer = findViewById(R.id.drawerLayout)
+
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        val navigationView: NavigationView = findViewById(R.id.nav_menu)
+        navigationView.setNavigationItemSelectedListener(this)
 
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val APaint = Intent(this, PaintActivity::class.java)
+        val ACalcul = Intent(this, CalculatorActivity::class.java)
+        val ACalender = Intent(this, CalendrierActivity::class.java)
+
+        when (item.itemId) {
+            R.id.paint -> startActivity(APaint)
+            R.id.calculatrice -> startActivity(ACalcul)
+            R.id.calendrier -> startActivity(ACalender)
+        }
+        return true
+    }
+
 }
